1f9a6d24b45d0f222625630b7662fa7689afb46a
Sverre Johann Bjørke <sverrejb@gmail.com>
2023-10-16 22:42:52 +0200

    data

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
-import time
+import json
 
 app = Flask(__name__)
+
+
+
+def load_json_file(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+database = load_json_file("static/fixtures.json")
+
 
 @app.route('/')
 def root():
