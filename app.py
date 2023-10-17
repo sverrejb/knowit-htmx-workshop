0889629f353d1abdd28d7090f149c7b67c44b0a1
Sverre Johann Bjørke <sverrejb@gmail.com>
2023-10-18 00:09:58 +0200

    edit works

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, render_template_string, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
@@ -8,6 +8,7 @@ import json
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "this is my secret key"
+PAGE_SIZE = 20
 
 
 def load_json_file(file_path):
@@ -25,31 +26,72 @@ class RegistrationForm(FlaskForm):
     job = StringField('Job', validators=[DataRequired()])
     submit = SubmitField('Register')
 
+class EditForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    job = StringField('Job', validators=[DataRequired()])
+    submit = SubmitField('Save')
 
 @app.route('/')
 def root():
     return render_template('index.html')
 
-
-@app.route('/employees')
-def employees():
-    page = request.args.get('page', 1, type=int)
-    start = (page-1)*20
-    end = start + 20
-    next_page = url_for('employees', page=page+1)
-    return render_template('employees.html', employees=database[start:end], next_page=next_page)
-
-
-@app.route('/registration', methods=['GET', 'POST'])
-def register():
+@app.route('/employees', methods=['POST'])
+def register_employee():
     form = RegistrationForm()
     if form.validate_on_submit():
         name = form.name.data
         year_of_birth = form.year_of_birth.data
         job = form.job.data
-        database.append(
-            {"name": name, "year_of_birth": year_of_birth, "job": job})
-        print(database)
+        database.insert(0, 
+            {"id": len(database) + 1, "name": name, "year_of_birth": year_of_birth, "job": job})
+    return render_template('employee_registred.html', name=name)
+
+
+
+@app.route('/employees')
+def get_employees():
+    employees = database
+    search_query = request.args.get('q', type=str)
+    if search_query:
+        employees = [employee for employee in employees if search_query.lower() in employee['name'].lower()]     
+        
+    page = request.args.get('page', 1, type=int)
+    start = (page-1)*PAGE_SIZE
+    end = start + PAGE_SIZE
+
+    next_page = url_for('get_employees', q=search_query, page=page+1)
+    if end >= len(employees):
+        next_page = None
+
+    return render_template('employees.html', employees=employees[start:end], query=search_query, next_page=next_page)
+
+@app.route('/employees/<int:id>')
+def get_employee(id):
+    form = EditForm()
+    for employee in database:
+        if employee['id'] == id:
+            return render_template('employee.html', employee=employee, form=form)
+    return render_template_string('PageNotFound {{ errorCode }}', errorCode='404'), 404
+
+@app.route('/employees/<int:id>', methods=["POST"])
+def update_employee(id):
+    form = EditForm()
+    print("yo")
+
+    if form.validate_on_submit():
+        for employee in database:
+            if employee['id'] == id:
+                print("wow")
+                employee["name"] = form.name.data
+                employee["job"] = form.job.data
+                return render_template_string("yay")
+
+    return render_template_string('PageNotFound {{ errorCode }}', errorCode='404'), 404
+
+
+@app.route('/registration', methods=['GET'])
+def register():
+    form = RegistrationForm()
     return render_template('register.html', form=form)
 
 
