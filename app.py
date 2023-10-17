3a68974e4ff244d5a84235b46407b5e8dc405f00
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-10-17 10:09:15 +0200

    some styling, added pagination

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, url_for
 import json
 
 app = Flask(__name__)
-
 
 
 def load_json_file(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
+
 
 database = load_json_file("static/fixtures.json")
 
@@ -17,13 +17,20 @@ database = load_json_file("static/fixtures.json")
 def root():
     return render_template('index.html')
 
+
 @app.route('/employees')
 def employees():
-    return render_template('employees.html', employees = database)
+    page = request.args.get('page', 1, type=int)
+    start = (page-1)*20
+    end = start + 20
+    next_page = url_for('employees', page=page+1)
+    return render_template('employees.html', employees=database[start:end], next_page=next_page)
+
 
 @app.route('/registration')
 def register():
     return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
